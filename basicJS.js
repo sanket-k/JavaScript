5ab2116db39d813014b4e9122191d840e80c9c39
sanket-k <sanket0249@gmail.com>
2017-09-02 14:18:28 +0530

    if-part-1

--- basicJS.js
+++ basicJS.js
@@ -1,1 +1,14 @@
-alert("hello world");
+var time_hrs = new Date().getHours();
+alert(time_hrs);
+
+if ( time_hrs <= 11){
+	document.getElementById("disp").innerHTML = "morning";
+}
+else if(time_hrs > 11 || time_hrs < 16){
+	document.getElementById("disp").innerHTML = "afternoon"; 
+}
+else if(time_hrs > 16 || time_hrs < 19){
+	document.getElementById("disp").innerHTML = "evening"; 
+}if(time_hrs > 19 ){
+	document.getElementById("disp").innerHTML = "night"; 
+}
